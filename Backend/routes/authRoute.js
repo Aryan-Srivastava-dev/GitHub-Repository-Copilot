b93abca97c81496a5ee782552f56aa0f53bbde09
Aryan Srivastava <aryansri8.11.05@gmail.com>
2026-08-10 18:51:54 +0530

    Add Dashboard auth

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,8 +1,11 @@
 import express from "express";
 import { login, callback } from "../controllers/authController.js";
+import { authenticate } from "../middleware/auth.js";
+import { dashboard } from "../controllers/dashboardController.js";
 const router = express.Router();
 
 router.get("/github/login", login);
 router.get("/github/callback", callback);
+router.get("/dashboard", authenticate, dashboard);
 
 export default router;
